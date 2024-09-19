57b19ed0b157a3f7d28565693c460ea20b674b87
26-AMoore <26amoore@derryfield.org>
2024-09-19 16:25:08 -0400

    added example plugin and OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ExampleOpMode.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.OpModeGroups;
+import org.firstinspires.ftc.teamcode.RobotPlugin;
+import org.firstinspires.ftc.teamcode.plugins.ExamplePlugin;
+
+/*
+ * This is an example OpMode, it should give basic structure to how an OpMode is formatted and
+ * interacts with plugins. The plugin used is the ExamplePlugin which should print "Hello Plugins!"
+ * then become a basic tank drive
+ */
+@TeleOp(name = "ExampleOpMode", group = OpModeGroups.SAMPLES) //Name and group of OpMode
+public class ExampleOpMode extends PluginOpMode { //All OpModes extend PluginOpMode
+	/*
+	 * OpModes should only have one function, initPlugins(). It returns an Array of RobotPlugins
+	 */
+	@Override //denotes we are overriding the default function, not strictly needed but is convention
+	public RobotPlugin[] initPlugins() {
+		//This is where we put our OpModes
+		ExamplePlugin examplePlugin = new ExamplePlugin(this); //instantiating our plugin
+		//If we wanted to add another plugin, we would do:
+//		OtherPlugin otherPlugin = new OtherPlugin(this);
+
+		//This is the return statement, where we return our Array of RobotPlugins
+		return new RobotPlugin[] { examplePlugin };
+		//if we were using our other plugin we would say
+//		return new RobotPlugin[] { examplePlugin, otherPlugin };
+	}
+}
+
+//And now you are done!
+//This is the whole OpMode!
