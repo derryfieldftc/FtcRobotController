1a2a46bbfdee80b35251d8bcd7d2d9ef1955274e
26-AMoore <26amoore@derryfield.org>
2024-09-26 10:21:16 -0400

    working on odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OdometryTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OdometryTestOpMode.java
@@ -1,18 +1,19 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.OpModeGroups;
 import org.firstinspires.ftc.teamcode.RobotPlugin;
 import org.firstinspires.ftc.teamcode.plugins.Claw;
 import org.firstinspires.ftc.teamcode.plugins.MecanumDrive;
+import org.firstinspires.ftc.teamcode.plugins.Odometry;
 
-@TeleOp(name = "ClawTestOpMode", group = OpModeGroups.TESTS)
-public class ClawTestOpMode extends PluginOpMode {
+@TeleOp(name = "OdometryTestOpMode", group = OpModeGroups.TESTS)
+public class OdometryTestOpMode extends PluginOpMode {
         @Override
         public RobotPlugin[] initPlugins() {
-			Claw claw = new Claw();
-            return new RobotPlugin[]{ claw };
+			Odometry odometry = new Odometry(this);
+			MecanumDrive mecanumDrive = new MecanumDrive(this);
+            return new RobotPlugin[]{ odometry, mecanumDrive };
         }
 }
