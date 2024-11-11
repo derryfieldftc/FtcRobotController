9d994fafe26cfabbd61a0ce11c508f5cdd1f838a
26-AMoore <26amoore@derryfield.org>
2024-11-11 10:32:59 -0500

    gs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ITDMotorsAndServoTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.OpModeGroups;
+import org.firstinspires.ftc.teamcode.RobotPlugin;
+import org.firstinspires.ftc.teamcode.plugins.DeviceTest;
+import org.firstinspires.ftc.teamcode.plugins.MecanumDrive;
+
+@TeleOp(name="Motor and Servo Test", group = OpModeGroups.TESTS)
+public class ITDMotorsAndServoTest extends PluginOpMode {
+	@Override
+	public RobotPlugin[] initPlugins() {
+		DeviceTest deviceTest = new DeviceTest(this, "slideH", "claw", "bucket", "rotate", "hinge");
+		MecanumDrive mecanumDrive = new MecanumDrive(this);
+		return new RobotPlugin[] { deviceTest, mecanumDrive };
+	}
+}
