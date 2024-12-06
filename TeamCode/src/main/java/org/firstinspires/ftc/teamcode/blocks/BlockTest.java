4b1571e3addcd1948fa0efd870283184f1522b3b
26-AMoore <26amoore@derryfield.org>
2024-12-06 10:17:27 -0500

    WORKS HECK YEAH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blocks/BlockTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blocks/BlockTest.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teamcode.blocks;
 
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
+import org.firstinspires.ftc.robotcore.external.ExportClassToBlocks;
 import org.firstinspires.ftc.robotcore.external.ExportToBlocks;
 
+@ExportClassToBlocks
 public class BlockTest extends BlocksOpModeCompanion {
+	public static enum Plugins {
+		MEC,
+		CLAW,
+		TANK
+	}
 	@ExportToBlocks
-	public static void sayHai() {
-		telemetry.addLine("Hai");
+	public static void addPlugin(String name, Plugins plugins) {
 	}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blocks/Plugins.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blocks/Plugins.java
@@ -2,17 +2,18 @@ package org.firstinspires.ftc.teamcode.blocks;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.ftccommon.external.WebHandlerRegistrar;
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
+import org.firstinspires.ftc.robotcore.external.ExportClassToBlocks;
 import org.firstinspires.ftc.robotcore.external.ExportToBlocks;
 import org.firstinspires.ftc.teamcode.RobotPlugin;
 import org.firstinspires.ftc.teamcode.plugins.*;
 
-import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 
+@ExportClassToBlocks
 public class Plugins extends BlocksOpModeCompanion {
-	public static ArrayList<RobotPlugin> plugins;
+	private static ArrayList<RobotPlugin> plugins = new ArrayList<>();
+
 	@ExportToBlocks
 	public static void usePlugin(String name) {
 		RobotPlugin addedPlugin;
@@ -20,6 +21,7 @@ public class Plugins extends BlocksOpModeCompanion {
 			addedPlugin = (RobotPlugin) Class.forName("org.firstinspires.ftc.teamcode.plugins." + name).getConstructor(OpMode.class).newInstance(opMode);;
 
 			plugins.add(addedPlugin);
+			telemetry.addLine("Init: " + name + ((addedPlugin == null) ? "null" : addedPlugin.toString()));
 			addedPlugin.init();
 		} catch (Exception e) {
 			telemetry.addLine("Uh Oh, The plugin " + name + " was not found or had some other error\n" + e.getMessage());
@@ -27,9 +29,35 @@ public class Plugins extends BlocksOpModeCompanion {
 	}
 
 	@ExportToBlocks
-	public static void updatePlugins() {
+	public static void initLoopPlugins() {
+		for (RobotPlugin plugin : plugins) {
+			plugin.init_loop();
+		}
+	}
+
+	@ExportToBlocks
+	public static void loopPlugins() {
 		for (RobotPlugin plugin : plugins) {
 			plugin.loop();
 		}
+	}
+
+	@ExportToBlocks
+	public static void startPlugins() {
+		for (RobotPlugin plugin : plugins) {
+			plugin.start();
+		}
+	}
+
+	@ExportToBlocks
+	public static void stopPlugins() {
+		for (RobotPlugin plugin : plugins) {
+			plugin.stop();
+		}
+	}
+
+	@ExportToBlocks
+	public static String getPlugins() {
+		return plugins.toString();
 	}
 }
