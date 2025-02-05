837d75e4734a8655dc394b633cf6b18155651478
tomeng70 <tomeng70@users.noreply.github.com>
2025-02-05 10:55:38 -0500

    initial BallBot code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ballbot/BallBot.java
@@ -0,0 +1,183 @@
+package org.firstinspires.ftc.teamcode.ballbot;
+
+import static androidx.core.math.MathUtils.clamp;
+import static org.firstinspires.ftc.teamcode.binarybot.BinaryBot.State.SPECIMEN_BACK_OFF;
+import static org.firstinspires.ftc.teamcode.binarybot.BinaryBot.State.SPECIMEN_HOOKING;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.binarybot.Manipulator;
+
+public class BallBot {
+    // ******************************************************************
+    // enumerations.
+    // ******************************************************************
+
+    // ******************************************************************
+    // Constants
+    // ******************************************************************
+
+    // ******************************************************************
+    // private member variables
+    // ******************************************************************
+    // drive system
+    private DcMotor motorFL;
+    private DcMotor motorBL;
+    private DcMotor motorFR;
+    private DcMotor motorBR;
+
+    RevHubOrientationOnRobot orientationOnRobot;
+
+    // op mode related items.
+    private OpMode opMode;
+    private HardwareMap hardwareMap;
+
+    private BNO055IMU imu;
+    private Orientation angles;
+    private Acceleration gravity;
+    private double previousAngle = 0;
+    public double integratedAngle = 0;
+
+    private final boolean USE_AUTO_CORRECT = true;
+    private final double P_COEFFICIENT_DRIVE = 0.1;
+    private final double P_COEFFICIENT_STRAFE = 0.05;
+
+    public BallBot(HardwareMap hardwareMap, OpMode opMode) {
+        this.opMode = opMode;
+        this.hardwareMap = hardwareMap;
+        initHardware();
+    }
+
+    private void initHardware() {
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+
+        motorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFR.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        initIMU();
+    }
+
+    private void initIMU() {
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = false;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+
+    public void updateAngles() {
+        float currAngle;
+        double deltaAngle;
+
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        currAngle = angles.firstAngle;
+
+        // the binary bot team uses the convention that a positive angle is a clockwise rotation.
+        // we need to flip the currAngle to work with this convention.
+        currAngle = - currAngle;
+
+        deltaAngle = currAngle - previousAngle;
+        if (deltaAngle < -180) {
+            // Went from +180 to -180 (direction).
+            deltaAngle = deltaAngle + 360;
+        } else if (deltaAngle > 180) {
+            // Went from -180 to +180 (direction).
+            deltaAngle = deltaAngle - 360;
+        }
+        integratedAngle = integratedAngle + deltaAngle;
+        previousAngle = currAngle;
+    }
+
+    public void resetAngles() {
+        integratedAngle = 0;
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        previousAngle = angles.firstAngle;
+
+        // the binary bot team uses the convention that a positive angle is a clockwise rotation.
+        // we need to flip the currAngle to work with this convention.
+        previousAngle = - previousAngle;
+    }
+
+    public double getCurrentAngle() {
+        return integratedAngle;
+    }
+
+    public double getPreviousAngle() {
+        return previousAngle;
+    }
+
+    // ******************************************************************
+    // public methods
+    // ******************************************************************
+    /**
+     * mecanum drive function.
+     *
+     * @param drive forward / backward (negative) power
+     * @param strafe rightwards / leftwards (negative) power
+     * @param twist clockwise / counterclockwise (negative) power
+     */
+    public void drive(double drive,  double strafe, double twist) {
+        double powerFL = drive + strafe + twist;
+        double powerBL = drive - strafe + twist;
+        double powerFR = drive - strafe - twist;
+        double powerBR = drive + strafe - twist;
+
+        powerFL = clamp(1, -1, powerFL);
+        powerBL = clamp(1, -1, powerBL);
+        powerFR = clamp(1, -1, powerFR);
+        powerBR = clamp(1, -1, powerBR);
+
+        motorFL.setPower(powerFL);
+        motorBL.setPower(powerBL);
+        motorFR.setPower(powerFR);
+        motorBR.setPower(powerBR);
+    }
+
+    /**
+     * Stop the robot.
+     */
+    public void stop() {
+        drive(0, 0, 0);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleBallBot.java
@@ -0,0 +1,86 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.teleop;
+
+import static org.firstinspires.ftc.teamcode.ballbot.EnhancedGamepad.Button.*;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.ballbot.BallBot;
+import org.firstinspires.ftc.teamcode.ballbot.EnhancedGamepad;
+
+@TeleOp(name="ITD Binary Bot", group="BinaryBot")
+//@Disabled
+public class TeleBallBot extends LinearOpMode {
+    private BallBot bot = null;
+    private EnhancedGamepad epad2 = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // create a new robot object.
+        bot = new BallBot(hardwareMap, this);
+
+        // get an enhanced gamepad for driver #2.
+        epad2 = new EnhancedGamepad(gamepad2);
+        bot.resetAngles();
+
+        // wait for start command from driver hub.
+        waitForStart();
+
+        // loop until opmode is stopped.
+        while (opModeIsActive()) {
+            // get gamepad input for mecanum drive.
+            double drive = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double twist = gamepad1.right_stick_x;
+
+            // scale power?
+            double scale = Math.abs(1 - gamepad1.right_trigger);
+            drive = scale * drive;
+            strafe = scale * strafe;
+            twist = scale * twist;
+
+            // update the mecanum drive.
+            bot.drive(drive, strafe, twist);
+
+            // use enhanced gamepad to detect if buttons were just pressed.
+            // update the enhanced gamepad data.
+            epad2.poll();
+
+            bot.updateAngles();
+            telemetry.addData("integrated angle", bot.integratedAngle);
+            telemetry.update();
+        }
+    }
+}
