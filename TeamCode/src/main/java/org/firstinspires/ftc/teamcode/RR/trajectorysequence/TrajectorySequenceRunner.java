d7f990f565a86e207d659e982f886057b849246c
26-AMoore <26amoore@derryfield.org>
2025-09-22 08:35:12 -0400

    Refactored

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR/trajectorysequence/TrajectorySequenceRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR/trajectorysequence/TrajectorySequenceRunner.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.trajectorysequence;
+package org.firstinspires.ftc.teamcode.RR.trajectorysequence;
 
 import androidx.annotation.Nullable;
 
@@ -17,13 +17,13 @@ import com.acmerobotics.roadrunner.trajectory.TrajectoryMarker;
 import com.acmerobotics.roadrunner.util.NanoClock;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
-import org.firstinspires.ftc.teamcode.drive.DriveConstants;
-import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.SequenceSegment;
-import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.TrajectorySegment;
-import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.TurnSegment;
-import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.WaitSegment;
-import org.firstinspires.ftc.teamcode.util.DashboardUtil;
-import org.firstinspires.ftc.teamcode.util.LogFiles;
+import org.firstinspires.ftc.teamcode.RR.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.RR.trajectorysequence.sequencesegment.SequenceSegment;
+import org.firstinspires.ftc.teamcode.RR.trajectorysequence.sequencesegment.TrajectorySegment;
+import org.firstinspires.ftc.teamcode.RR.trajectorysequence.sequencesegment.TurnSegment;
+import org.firstinspires.ftc.teamcode.RR.trajectorysequence.sequencesegment.WaitSegment;
+import org.firstinspires.ftc.teamcode.RR.util.DashboardUtil;
+import org.firstinspires.ftc.teamcode.RR.util.LogFiles;
 
 import java.util.ArrayList;
 import java.util.Collections;
