e242008cee6a33e5991b7ff367dc6f108b98fb9a
26-AMoore <26amoore@derryfield.org>
2025-09-23 22:34:10 -0400

    meepmeep inital testing

---
+++ MeepMeep/src/main/java/MeepMeepTest.java
@@ -0,0 +1,40 @@
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class MeepMeepTest {
+	public static void main(String[] args) {
+		MeepMeep meepMeep = new MeepMeep(800);
+
+		RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+				// Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+				.setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 16)
+				.followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+						.splineTo(new Pose2d(48, 0, Math.toRadians(0)), Math.toRadians(0))
+						.splineTo(new Pose2d(48, 48, Math.toRadians(0)), Math.toRadians(0))
+						.splineTo(new Pose2d(0, 48, Math.toRadians(0)), Math.toRadians(0))
+						.splineTo(new Pose2d(0, 0, Math.toRadians(0)), Math.toRadians(0))
+						.build());
+
+
+		Image img = null;
+		try {
+			img = ImageIO.read(new File("MeepMeep/src/main/res/Decode.png"));
+		} catch (IOException e) {
+		}
+
+		meepMeep.setBackground(img)
+				.setDarkMode(true)
+				.setBackgroundAlpha(0.95f)
+				.addEntity(myBot)
+				.start();
+	}
+}
