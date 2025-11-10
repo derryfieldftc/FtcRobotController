b10697b275be23bbf879ddd99a513cc5d8d39692
26-AMoore <26amoore@derryfield.org>
2025-11-10 14:39:44 -0500

    inital untuned pedro pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -3,17 +3,46 @@ package org.firstinspires.ftc.teamcode.pedro;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.ThreeWheelConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants();
+    public static MecanumConstants mecanumDrive = new MecanumConstants().maxPower(1)
+            .rightFrontMotorName("motorFR")
+            .rightRearMotorName("motorBR")
+            .leftRearMotorName("motorBL")
+            .leftFrontMotorName("motorFL")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+    public static ThreeWheelConstants threeWheelLocalizer = new ThreeWheelConstants()
+            .forwardTicksToInches(.001989436789)
+            .strafeTicksToInches(.001989436789)
+            .turnTicksToInches(.001989436789)
+            .leftPodY(8)
+            .rightPodY(-8)
+            .strafePodX(-0)
+            .leftEncoder_HardwareMapName("motorBL")
+            .rightEncoder_HardwareMapName("intake")
+            .strafeEncoder_HardwareMapName("motorBR")
+            .leftEncoderDirection(Encoder.FORWARD)
+            .rightEncoderDirection(Encoder.FORWARD)
+            .strafeEncoderDirection(Encoder.REVERSE);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .mecanumDrivetrain(mecanumDrive)
                 .pathConstraints(pathConstraints)
+                .threeWheelLocalizer(threeWheelLocalizer)
                 .build();
     }
 }
