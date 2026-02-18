df9e1f920b2094680bc55cfc14fef3724c41c185
26-AMoore <26amoore@derryfield.org>
2026-02-18 11:38:33 -0500

    added sloth

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedOpMode.java
@@ -118,8 +118,7 @@ public class RedOpMode extends OpMode {
 
 		bot.intake.setSpeed(gamepad2.right_trigger * ((gamepad2.start) ? -1 : 1));
 		if (gamepad2.right_trigger > .1 && !gamepad2.start) {
-			bot.spindexerPrepIntake().run(); // bad solution but works for now
-			bot.spindexerPrepIntake().run();
+			while (bot.spindexer.resetPosition().run()); // its okay to have a while loop here because this executes really really fast
 			bot.spindexer.setLiftPosition(Spindexer.Height.Down);
 		} else {
 			bot.spindexer.setLiftPosition(Spindexer.Height.Up);
