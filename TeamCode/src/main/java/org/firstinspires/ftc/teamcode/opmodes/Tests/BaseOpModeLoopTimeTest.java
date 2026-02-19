04977616b56bc2bc086caf33830e79754f49d992
26-AMoore <26amoore@derryfield.org>
2026-02-19 13:49:50 -0500

    disabled useless tests and updated teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tests/BaseOpModeLoopTimeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tests/BaseOpModeLoopTimeTest.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmodes.Tests;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
+@Disabled
 public class BaseOpModeLoopTimeTest extends OpMode {
     double lastLoopTime;
     double averageLoopTime;
